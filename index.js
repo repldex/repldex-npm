9f505c93b72f77d1fcd69dadf869d4120c2aa36b
selectthegang <ceo@selectthegang.tk>
2021-11-22 20:48:38 -0600

    Removed constructor from index.js

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const fetch = require('node-fetch');
 class Client {
   /**
    * Initiates Class.
-   * @param {String} token Your Repldex Token
    */
-  constructor(token) {
-    if (token) this.token = token;
-    else throw new Error("You didn't specify a token in constructor!");
-  }
 
 }
